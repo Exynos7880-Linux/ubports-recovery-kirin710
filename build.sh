67d98c96f31fe2044e024f64cc5abf7181f3de88
aymanrgab <aymanrar2c@gmail.com>
2022-12-05 17:25:15 +0200

    initial commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source hadk.env
+cd $ANDROID_ROOT
+
+./hybris-patches/apply-patches.sh --mb
+
+source build/envsetup.sh 2>&1
+breakfast $DEVICE
+
+echo "clean .repo folder"
+rm -rf $ANDROID_ROOT/.repo
+
+make -j$(nproc --all) recoveryramdisk
