86860ca4fba2725280a7f7456de203803ade237e
aymanrgab <aymanrar2c@gmail.com>
2022-12-06 11:15:32 +0200

    build a7 recovery

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 source hadk.env
 cd $ANDROID_ROOT
 
+sed -i '/^[^#]/ s/\(^.*CACHE.*$\)/#\ \1/' device/samsung/universal7880-common/ramdisk/fstab.samsungexynos7880
+
 ./hybris-patches/apply-patches.sh --mb
 
 source build/envsetup.sh 2>&1
@@ -11,4 +13,8 @@ breakfast $DEVICE
 echo "clean .repo folder"
 rm -rf $ANDROID_ROOT/.repo
 
+make -j$(nproc --all) recoveryimage
+
+git clone -b lineage-17.1 https://github.com/LineageOS/android_device_samsung_a7y17lte.git device/samsung/a7y17lte
+breakfast a7y17lte
 make -j$(nproc --all) recoveryimage
