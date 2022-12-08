3a658992655cf3262f6693cacdb5c8057dc4ad5d
aymanrgab <aymanrar2c@gmail.com>
2022-12-08 18:07:22 +0200

    my bad lol

--- build_a7.sh
+++ build_a7.sh
@@ -2,14 +2,14 @@
 source hadk_a7.env
 cd $ANDROID_ROOT
 
-sed -i '10d' ~/halium/device/samsung/universal7880-common/ramdisk/fstab.samsungexynos7880
-sed -i '4d' ~/halium/device/samsung/universal7880-common/twrp/twrp.fstab
-
 ./hybris-patches/apply-patches.sh --mb
 
 source build/envsetup.sh 2>&1
 #lunch $DEVICE
 breakfast $DEVICE
+
+sed -i '10d' ~/halium/device/samsung/universal7880-common/ramdisk/fstab.samsungexynos7880
+sed -i '4d' ~/halium/device/samsung/universal7880-common/twrp/twrp.fstab
 
 echo "clean .repo folder"
 rm -rf $ANDROID_ROOT/.repo
