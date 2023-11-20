0e25aa598cfca6814440fa54262f1fcd04ae16be
Ayman Ragab <45921642+aymanrgab@users.noreply.github.com>
2023-11-20 17:12:40 +0000

    update

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 source hadk.env
 cd $ANDROID_ROOT
 
+git clone https://github.com/Exynos7880-Linux/android_vendor_huawei_kirin710-9-common.git vendor/huawei/kirin710-9-common
+git clone https://github.com/Exynos7880-Linux/android_vendor_huawei_kirin710.git vendor/huawei/kirin710
+git clone https://github.com/Exynos7880-Linux/android_device_huawei_kirin710-9-common.git device/huawei/kirin710-9-common
+git clone https://github.com/Exynos7880-Linux/android_device_huawei_kirin710.git device/huawei/kirin710
+git clone https://github.com/Exynos7880-Linux/kernel_huawei_miami.git kernel/huawei/kirin710-9
+git clone https://github.com/Bakoubak/android_hardware_huawei.git hardware/huawei
+
 ./hybris-patches/apply-patches.sh --mb
 
 source build/envsetup.sh 2>&1
