492c50f8755c13722a51d1600910775842a0bd1d
Ayman Ragab <45921642+aymanrgab@users.noreply.github.com>
2023-11-22 05:12:59 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ git clone https://github.com/Exynos7880-Linux/android_vendor_huawei_kirin710-9-c
 git clone https://github.com/Exynos7880-Linux/android_vendor_huawei_kirin710.git vendor/huawei/kirin710
 git clone https://github.com/Exynos7880-Linux/android_device_huawei_kirin710-9-common.git device/huawei/kirin710-9-common
 git clone https://github.com/Exynos7880-Linux/android_device_huawei_kirin710.git device/huawei/kirin710
-git clone https://github.com/Bakoubak/android_kernel_huawei-kirin710-9.git kernel/huawei/kirin710-9
+git clone https://github.com/Exynos7880-Linux/android_kernel_huawei-kirin710-9.git kernel/huawei/kirin710-9
 git clone https://github.com/Exynos7880-Linux/android_hardware_huawei.git hardware/huawei
 #git clone https://github.com/Iceows/android_vendor_huawei_hi6250-9-common.git vendor/huawei/hi6250-9-common
 git clone https://android.googlesource.com/platform/hardware/broadcom/wlan hardware/broadcom/wlan -b android10-release
