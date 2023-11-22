78a02f70b1044a043219a4a8b603b1037c6583f6
Ayman Ragab <45921642+aymanrgab@users.noreply.github.com>
2023-11-22 08:45:09 +0000

    up

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ git clone https://github.com/Exynos7880-Linux/android_device_huawei_kirin710.git
 git clone https://github.com/Exynos7880-Linux/android_kernel_huawei-kirin710-9.git kernel/huawei/kirin710-9
 git clone https://github.com/Exynos7880-Linux/android_hardware_huawei.git hardware/huawei
 #git clone https://github.com/Iceows/android_vendor_huawei_hi6250-9-common.git vendor/huawei/hi6250-9-common
-git clone https://android.googlesource.com/platform/hardware/broadcom/wlan hardware/broadcom/wlan -b android10-release
-git clone https://android.googlesource.com/platform/external/wpa_supplicant_8 external/wpa_supplicant_8 -b android10-release
+#git clone https://android.googlesource.com/platform/hardware/broadcom/wlan hardware/broadcom/wlan -b android10-release
+#git clone https://android.googlesource.com/platform/external/wpa_supplicant_8 external/wpa_supplicant_8 -b android10-release
 
 ./hybris-patches/apply-patches.sh --mb
 
